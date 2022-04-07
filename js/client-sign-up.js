889fbea2efaaf3472b52714441c77e8fe29bc221
Alvaro Jose Santos <69470882+bipopa@users.noreply.github.com>
2022-04-07 17:16:16 -0500

    conexion backend registro javascript

---
+++ js/client-sign-up.js
@@ -0,0 +1,37 @@
+function registrar() {
+    let txt_nombre = $("#full-name").val();
+    let txt_username = $("#nickname").val();
+    let txt_email = $("#email").val();
+    let txt_age = $("#age").val();
+    let txt_country = $("#country").val();
+    let txt_password = $("#password").val();
+
+    console.log(txt_nombre);
+    console.log(txt_username);
+    console.log(txt_email);
+    console.log(txt_age);
+    console.log(txt_country);
+
+    $.ajax({
+        url:'http://localhost:3001/viewers',   // url
+        dataType: 'text/json',
+        type: 'post',
+        contentType: 'application/x-www-form-urlencoded',
+        data: {
+            name: txt_nombre,
+            username: txt_username,
+            email: txt_email,
+            password: txt_password,
+        }, // data to be submit
+        success: function( data, textStatus, jQxhr ){
+            alert("Guardado!!!");
+            cargar();
+            window.location.replace("./index.html");
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+            alert('Error al crear la cuenta');
+            window.location.replace("./login.html");
+        }
+    });
+}
