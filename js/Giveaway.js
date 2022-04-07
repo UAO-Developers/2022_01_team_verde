631bb07f75e2eea250c5566a8f9341c8f8f9d9f9
Alvaro Jose Santos <69470882+bipopa@users.noreply.github.com>
2022-04-07 16:08:32 -0500

    Add files via upload

---
+++ js/Giveaway.js
@@ -0,0 +1,51 @@
+function cargar() {
+    $.ajax({
+        url:'http://localhost:3001/api/sorteos',   // url
+        type: 'get',
+        success: function( data, textStatus, jQxhr ){
+            let html = "";
+            let i=1;
+            data.products.forEach(element => {
+                console.log(element);
+                
+                html += `
+                <tr>
+                                  <th scope="row">${i}</th>
+                                  <td>${element.StreamerId}</td>
+                                  <td>${element.nombreSorteo}</td>
+                                  <td>${element.date}</td>
+                                  <td>${element.premio}</td>
+                                  <td class="button-nav" onclick="borrar();">x</td>
+                                  <td class="button-nav" onclick="modificar();">mod</td>
+                                </tr>
+                `;
+            $("#giveaways_list").html(html);
+            i+=1
+            });
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+}
+function borrar(id){
+    $.ajax({
+        url:'http://localhost:3001/api/sorteos',   // url
+        dataType: 'text/json',
+        type: 'delete',
+        contentType: 'application/x-www-form-urlencoded',
+        data: {
+            id: nombreSorteo,
+        }, // data to be submit
+        success: function( data, textStatus, jQxhr ){
+            alert("Guardado!!!");
+            cargar();
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+}
+$(document).ready(function(){
+    cargar();
+});
